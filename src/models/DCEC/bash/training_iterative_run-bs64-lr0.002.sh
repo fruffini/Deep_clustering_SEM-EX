d58cd2d2ece0f1f5bdac5f279d5e1ea3d9b8274b
filippo ruffini <ruffin02@outlook.it>
2022-08-31 12:37:01 +0200

    k from 7 to 24

--- src/models/DCEC/bash/training_iterative_run-bs64-lr0.002.sh
+++ src/models/DCEC/bash/training_iterative_run-bs64-lr0.002.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #SBATCH -A SNIC2022-5-277  -p alvis
 #SBATCH -N 1 --gpus-per-node=V100:4
-#SBATCH -t 3-00:00:00
+#SBATCH -t 3-23:59:00
 # Output files
 #SBATCH --error=job_%J.err
 #SBATCH --output=out_%J.out
@@ -23,7 +23,7 @@ module load Python/3.8.6-GCCcore-10.2.0
 cd /mimer/NOBACKUP/groups/snic2022-5-277/fruffini/SEM-EX/src/models/DCEC
 
 # Train HERE YOU RUN YOUR PROGRAM
-python train.py --batch_size=128 --lr_tr=0.001 --phase train --verbose --dataset_name CLARO --k_0=4 --k_fin=24 --delta_check --update_interval=2000 --delta_label=0.001 --data_dir '/mimer/NOBACKUP/groups/snic2022-5-277/ltronchin/data' --config_dir '/mimer/NOBACKUP/groups/snic2022-5-277/fruffini/SEM-EX/configs' --reports_dir '/mimer/NOBACKUP/groups/snic2022-5-277/fruffini/SEM-EX/reports' --embedded_dimension=256 --AE_type CAE512 --n_epochs=500 --n_epochs_decay=500 --save_latest_freq=5000 --gpu_ids '0,1,2,3'
+python train.py --batch_size=128 --lr_tr=0.001 --phase train --verbose --dataset_name CLARO --k_0=7 --k_fin=24 --delta_check --update_interval=2000 --delta_label=0.001 --data_dir '/mimer/NOBACKUP/groups/snic2022-5-277/ltronchin/data' --config_dir '/mimer/NOBACKUP/groups/snic2022-5-277/fruffini/SEM-EX/configs' --reports_dir '/mimer/NOBACKUP/groups/snic2022-5-277/fruffini/SEM-EX/reports' --embedded_dimension=256 --AE_type CAE512 --n_epochs=500 --n_epochs_decay=500 --save_latest_freq=5000 --gpu_ids '0,1,2,3'
 
 # Deactivate venv
 deactivate
