3d573a1823d26937d6ba659819660837afe2ce00
filippo ruffini <ruffin02@outlook.it>
2022-10-24 14:54:52 +0200

    CLARO_DATASET.py print some images

--- src/models/DCEC/dataset/CLARO_dataset.py
+++ src/models/DCEC/dataset/CLARO_dataset.py
@@ -225,7 +225,6 @@ class CLARODataset(BaseDataset):
         """Generates one sample of data"""
         # Select sample
         row = self.data.iloc[index]
-
         row_name = row.name.split('_')
         img_id = row_name[1]
         patient_id = row_name[0]
@@ -235,6 +234,14 @@ class CLARODataset(BaseDataset):
         img_path = os.path.join(self.raw_dir, patient_id, "images", f"{patient_id + '_' + img_id}.tif")
         x = loader(img_path=img_path, img_dim=self.config['data']['image_size'],
                    box=box, clip=self.clip, scale=self.scale, convert_to_uint8=self.convert_to_uint8, scale_by_255=self.scale_by_255)
+        # DEBUG IMAGE
+        print(x.shape)
+        img = 255.0 * x
+        img = img.astype(np.uint8)
+        img = Image.fromarray(img[0, :, :])
+        img.save(
+            fp=os.path.join("/mimer/NOBACKUP/groups/snic2022-5-277/fruffini/Data_temp",f"IMG_{patient_id + img_id}.tif")
+        )
         if self.transform is not None:
             x = self.transform(x)
         return x, patient_id, img_id
