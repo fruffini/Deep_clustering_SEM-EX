0ff66678b8b75ccab15767b832222fa9d8b3c31c
filippo ruffini <ruffin02@outlook.it>
2022-11-03 16:33:11 +0100

    <BASH> , bash files correction A100

--- src/models/DCEC/bash/test_iterative_ID3.sh
+++ src/models/DCEC/bash/test_iterative_ID3.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #SBATCH -A SNIC2022-5-277  -p alvis
-#SBATCH -N 1 --gpus-per-node=V100:1
+#SBATCH -N 1 --gpus-per-node=A100:1
 #SBATCH -t 0-18:00:00
 # Output files
 #SBATCH --error=job_%J.err
